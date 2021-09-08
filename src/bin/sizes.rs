c90cbbe83ae4fe3a537f1585580d97458df5b23c
Leon <leon.botros@ru.nl>
2021-09-08 14:35:47 +0200

    use the same exported constants for Boyen-Waters, include in size benchmark

--- src/bin/sizes.rs
+++ src/bin/sizes.rs
@@ -1,7 +1,8 @@
 //! This file produces a binary that prints the sizes of several IBE/KEM components
 //! such as the MPK, MSK, USK, CT.
 
-macro_rules! print_sizes_kem {
+/// Most common call convention
+macro_rules! print_sizes_kem1 {
     ($scheme_name: ident) => {{
         use ibe::kem::$scheme_name::*;
 
@@ -26,35 +27,38 @@ macro_rules! print_sizes_kem {
     }};
 }
 
-/// Call conventions are slightly different for this scheme
-fn print_size_kiltz_vahlis_one() {
-    use ibe::kem::kiltz_vahlis_one::*;
-    let mut rng = rand::thread_rng();
+/// Slightly different convention: extract needs pk
+macro_rules! print_sizes_kem2 {
+    ($scheme_name: ident) => {{
+        use ibe::kem::$scheme_name::*;
 
-    let id = "email:w.geraedts@sarif.nl".as_bytes();
-    let kid = Identity::derive(id);
+        let mut rng = rand::thread_rng();
 
-    let (pk, sk) = setup(&mut rng);
-    let usk = extract_usk(&pk, &sk, &kid, &mut rng);
-    let (c, _k) = encaps(&pk, &kid, &mut rng);
+        let id = "email:w.geraedts@sarif.nl".as_bytes();
+        let kid = Identity::derive(id);
 
-    let pk_bytes = pk.to_bytes();
-    let sk_bytes = sk.to_bytes();
-    let usk_bytes = usk.to_bytes();
-    let ct_bytes = c.to_bytes();
+        let (pk, sk) = setup(&mut rng);
+        let usk = extract_usk(&pk, &sk, &kid, &mut rng);
+        let (c, _k) = encaps(&pk, &kid, &mut rng);
 
-    println!("kiltz_vahlis_one");
-    println!("MPK:\t{}", pk_bytes.len());
-    println!("MSK:\t{}", sk_bytes.len());
-    println!("USK:\t{}", usk_bytes.len());
-    println!("CT:\t{}\n", ct_bytes.len());
+        let pk_bytes = pk.to_bytes();
+        let sk_bytes = sk.to_bytes();
+        let usk_bytes = usk.to_bytes();
+        let ct_bytes = c.to_bytes();
+
+        println!(stringify!($scheme_name));
+        println!("MPK:\t{}", pk_bytes.len());
+        println!("MSK:\t{}", sk_bytes.len());
+        println!("USK:\t{}", usk_bytes.len());
+        println!("CT:\t{}\n", ct_bytes.len());
+    }};
 }
 
 fn main() {
-    println!("Sizes in bytes:\n");
-    print_size_kiltz_vahlis_one();
-
-    print_sizes_kem!(cgw_cpa);
-    print_sizes_kem!(cgw_cca_fo);
-    print_sizes_kem!(cgw_cca_kv);
+    println!("KEM sizes in bytes:\n");
+    print_sizes_kem1!(cgw_cpa);
+    print_sizes_kem1!(cgw_cca_fo);
+    print_sizes_kem1!(cgw_cca_kv);
+    print_sizes_kem2!(kiltz_vahlis_one);
+    print_sizes_kem2!(boyen_waters);
 }
